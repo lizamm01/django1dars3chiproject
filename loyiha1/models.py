947a37447df88e6914f2f5cfc7ed8beed22fed94
Muxlisa <muxlisa@gmail.com>
2025-07-15 18:58:17 +0500

    all files

--- loyiha1/models.py
+++ loyiha1/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+class Viloyat(models.Model):
+    title = models.CharField(max_length=100)
+    context = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+
+class Tuman(models.Model):
+    title = models.CharField(max_length=100)
+    context = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    viloyat = models.ForeignKey(Viloyat, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
+class Mahalla(models.Model):
+    title = models.CharField(max_length=100)
+    context = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    tuman = models.ForeignKey(Tuman, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
